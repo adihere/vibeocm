62c6625d9855b7a77fd6f244a51abd5896897a93
adihere <aditya.vadaganadam@gmail.com>
2025-04-05 20:05:10 +0100

    update code to comment out all posthog analytics temporarily

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -18,7 +18,7 @@ export function ThemeToggle() {
       onClick={() => {
         const newTheme = theme === "dark" ? "light" : "dark"
         setTheme(newTheme)
-        captureThemeChange(newTheme)
+        // captureThemeChange(newTheme) // Temporarily disabled
       }}
     >
       <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
